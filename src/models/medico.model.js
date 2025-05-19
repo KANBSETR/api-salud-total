839beb1f2c85ec15440645bf83c2bba311f2fbbe
KANBSETR <azobisiso576@gmail.com>
2025-05-19 00:20:56 -0400

    Implementación de algunos endpoints de médicos

--- src/models/medico.model.js
+++ src/models/medico.model.js
@@ -20,3 +20,20 @@ export const getMedicoById = async (id) => {
     }
     return result.rows[0];
 }
+
+export const getMedicoByIdEspecialidad = async (idEspecialidad) => {
+    const result = await pool.query("SELECT * FROM medico WHERE id_especialidad = $1", [idEspecialidad]);
+    if (result.rowCount === 0) {
+        throw new Error("Medico no encontrado");
+    }
+    return result.rows;
+}
+
+
+export const horarioMedico = async (id) => {
+    const result = await pool.query("SELECT * FROM horario WHERE id_medico = $1", [id]);
+    if (result.rowCount === 0) {
+        throw new Error("Horario no encontrado");
+    }
+    return result.rows[0];
+}
