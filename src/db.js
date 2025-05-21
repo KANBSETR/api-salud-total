8b4cb57481eec51947912be1fcd5057568e5b40b
KANBSETR <azobisiso576@gmail.com>
2025-05-21 14:43:22 -0400

    Cambio de conexion a producción

--- src/db.js
+++ src/db.js
@@ -1,22 +1,22 @@
 import pg from 'pg';
 import { PG_USER, PG_DATABASE, PG_HOST, PG_PASSWORD, PG_PORT} from './config.js';
 
-export const pool = new pg.Pool({
-    host: PG_HOST,
-    user: PG_USER,
-    password: PG_PASSWORD,
-    database: PG_DATABASE,
-    port: PG_PORT
-});
-
-// Produccion
 // export const pool = new pg.Pool({
 //     host: PG_HOST,
 //     user: PG_USER,
 //     password: PG_PASSWORD,
 //     database: PG_DATABASE,
-//     port: PG_PORT,
-//     ssl:{
-//         rejectUnauthorized: false
-//     }
+//     port: PG_PORT
 // });
+
+// Produccion
+export const pool = new pg.Pool({
+    host: PG_HOST,
+    user: PG_USER,
+    password: PG_PASSWORD,
+    database: PG_DATABASE,
+    port: PG_PORT,
+    ssl:{
+        rejectUnauthorized: false
+    }
+});
