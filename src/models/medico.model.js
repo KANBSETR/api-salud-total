b3520c1c257a3648508696e195e238a50ab1a406
KANBSETR <azobisiso576@gmail.com>
2025-05-25 00:39:56 -0400

    Query datos médicos

--- src/models/medico.model.js
+++ src/models/medico.model.js
@@ -21,14 +21,20 @@ export const getMedicoById = async (id) => {
     return result.rows[0];
 }
 
+// MEJORAR LA CONSULTA PARA TRAER LOS DATOS DEL MÉDICO
 export const getMedicoByIdEspecialidad = async (idEspecialidad) => {
-    const result = await pool.query("SELECT * FROM medico WHERE idEspecialidad = $1", [idEspecialidad]);
+    const query = `
+    SELECT md.idmedico, us.rut, us.correo, us.nombre, us.appaterno, us.apmaterno, md.idespecialidad
+    FROM usuario us 
+    JOIN medico md ON (md.idusuario = us.idusuario)
+    WHERE md.idespecialidad = $1;
+    `
+    const result = await pool.query(query, [idEspecialidad]);
     if (result.rowCount === 0) {
         throw new Error("Medico no encontrado");
     }
     return result.rows;
 }
-
 
 export const horarioMedico = async (id) => {
     const query = `
