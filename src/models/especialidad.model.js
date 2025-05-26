de85b0ee51cae20c307f7774ffcb9ce758559015
KANBSETR <azobisiso576@gmail.com>
2025-05-26 19:18:54 -0400

    Cambios menores...

--- src/models/especialidad.model.js
+++ src/models/especialidad.model.js
@@ -1,11 +1,11 @@
 import { pool } from '../db.js';
 
 export const getEspecialidades = async () => {
-    const result = await pool.query('SELECT * FROM especialidad');
+    const result = await pool.query('SELECT idespecialidad, nomespe FROM especialidad');
     return result;
 };
 
 export const getEspecialidadById = async (id) => {
-    const result = await pool.query('SELECT * FROM especialidad WHERE idespecialidad = $1', [id]);
+    const result = await pool.query('SELECT idespecialidad, nomespe FROM especialidad WHERE idespecialidad = $1', [id]);
     return result.rows[0];
 };
--- src/models/paciente.model.js
+++ src/models/paciente.model.js
@@ -1,6 +1,10 @@
 import { pool } from "../db.js";
 
 export const getPacientes = async () => {
-    const result = await pool.query("SELECT * FROM paciente");
+    const query = `
+    SELECT pc.idpaciente, us.rut, us.nombre, us.appaterno, us.apmaterno, us.idusuario
+    FROM usuario us JOIN paciente pc ON (us.idusuario = pc.idusuario)
+    `;
+    const result = await pool.query(query);
     return result.rows;
 }
